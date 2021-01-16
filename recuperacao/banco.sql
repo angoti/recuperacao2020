8eb5b3a7623e9df9459de573b61cfc47572d82d4
Edson Angoti Junior <angoti@gmail.com>
2021-01-15 22:20:21 -0300

    script sql para criação do banco de dados e tabela.

---
+++ recuperacao/banco.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE `recuperacao` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE TABLE `alunos` (
+  `idalunos` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idalunos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
