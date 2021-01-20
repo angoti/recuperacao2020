b264f71c30f8106b741782f88cebdd9e009c1999
Edson Angoti Junior <angoti@gmail.com>
2021-01-20 17:06:50 -0300

    listagem de alunos

--- src/main/java/com/angoti/recuperacao/controle/Controlador.java
+++ src/main/java/com/angoti/recuperacao/controle/Controlador.java
@@ -1,7 +1,12 @@
 package com.angoti.recuperacao.controle;
 
+import com.angoti.recuperacao.dao.AlunoDAO;
+import com.angoti.recuperacao.dominio.Aluno;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class Controlador {
@@ -12,7 +17,10 @@ public class Controlador {
 	}
 
 	@GetMapping("/crud")
-	public String crud() {
+	public String crud(Model modelo) {
+		AlunoDAO alunoDAO = new AlunoDAO();
+		List<Aluno> lista = alunoDAO.buscarTodos();
+		modelo.addAttribute("alunos",lista);
 		return "index";
 	}
 
