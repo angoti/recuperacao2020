ab14fbb81cbe2077a9ee4561bb21ccd8a03cd65d
Edson Angoti Junior <angoti@gmail.com>
2021-01-20 13:10:39 -0300

    ajustes git

---
+++ src/test/java/com/angoti/recuperacao/AppRecuperacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angoti.recuperacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRecuperacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
